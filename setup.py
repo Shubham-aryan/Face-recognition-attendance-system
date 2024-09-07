251859c02a64e23286728f15186eff73f03e7709
Shubham Kumar <shubhamkumar1428@gmail.com>
2024-09-07 20:05:46 +0530

    Adding Files

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
 os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
 os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
 
+// none
 base = None
 
 if sys.platform == 'win32':
